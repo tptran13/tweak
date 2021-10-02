c3982534b9ae9dca055a2fb4d34c11a96ebe11ea
Tho Tran <tptran13@neiu.edu>
2021-10-02 00:40:05 -0500

    Initial commit

---
+++ src/main/java/edu/neiu/tweak/TweakApplication.java
@@ -0,0 +1,15 @@
+package edu.neiu.tweak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweakApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TweakApplication.class, args);
+    }
+
+}
