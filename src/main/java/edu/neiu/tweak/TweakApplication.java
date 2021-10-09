c0acc53785d65c227f9ab6fd537e15e2fbdfe743
Tho Tran <tptran13@neiu.edu>
2021-10-08 19:31:39 -0500

    Created class CreateProfile in package model

--- src/main/java/edu/neiu/tweak/TweakApplication.java
+++ src/main/java/edu/neiu/tweak/TweakApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TweakApplication
 {
-
     public static void main(String[] args)
     {
         SpringApplication.run(TweakApplication.class, args);
---
+++ src/main/java/edu/neiu/tweak/model/CreateProfile.java
@@ -0,0 +1,83 @@
+package edu.neiu.tweak.model;
+
+public class CreateProfile
+{
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String username;
+    private String password;
+    private String email;
+
+    public CreateProfile(){};
+
+    public CreateProfile(String firstName, String lastName, String dateOfBirth, String username, String password, String email)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getDateOfBirth()
+    {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth)
+    {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+}
