90e5f545f156a1abe9c5cd701f4f642c48325ee9
Tho Tran <tptran13@neiu.edu>
2021-10-08 21:58:55 -0500

    Created add-createProfile html file and added @PostMapping to CreateProfileController to redirect to index-page.

---
+++ src/main/java/edu/neiu/tweak/controller/CreateProfileController.java
@@ -0,0 +1,34 @@
+package edu.neiu.tweak.controller;
+
+import edu.neiu.tweak.model.CreateProfile;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/createprofile")
+public class CreateProfileController
+{
+    @GetMapping
+    public String getCreateProfile(Model model)
+    {
+        model.addAttribute("newProfile", new CreateProfile());
+        return "add-createprofile";
+    }
+
+    @PostMapping
+    public String handleCreateProfileForm(@ModelAttribute("newProfile") CreateProfile profile)
+    {
+        System.out.println("First name: " + profile.getFirstName());
+        System.out.println("Last name: " + profile.getLastName());
+        System.out.println("Date of Birth:" + profile.getDateOfBirth());
+        System.out.println("Username: " + profile.getUsername());
+        System.out.println("Password: " + profile.getPassword());
+        System.out.println("Email: " + profile.getEmail());
+
+        return "redirect:/indexpage";
+    }
+}
--- src/main/java/edu/neiu/tweak/controller/HomeController.java
+++ src/main/java/edu/neiu/tweak/controller/HomeController.java
@@ -1,15 +1,17 @@
 package edu.neiu.tweak.controller;
 
+import edu.neiu.tweak.model.CreateProfile;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/indexpage")
 public class HomeController
 {
     @GetMapping
-    public String getHomePage()
+    public String getHomePage(Model model)
     {
         return "index-page";
     }
