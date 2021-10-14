40c6b3b1962bed80fcf0470571a3655c085b31a4
Tho Tran <tptran13@neiu.edu>
2021-10-13 23:40:26 -0500

    Added profile-registered xml file and redirected post mapping /createprofile/welcome

--- src/main/java/edu/neiu/tweak/controller/CreateProfileController.java
+++ src/main/java/edu/neiu/tweak/controller/CreateProfileController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/createprofile")
@@ -19,16 +20,23 @@ public class CreateProfileController
         return "add-createprofile";
     }
 
-    @PostMapping
-    public String handleCreateProfileForm(@ModelAttribute("newProfile") CreateProfile profile)
+    @GetMapping("/welcome")
+    public String displayThankYou(@ModelAttribute("name") Object attr, Model model)
     {
-        System.out.println("First name: " + profile.getFirstName());
-        System.out.println("Last name: " + profile.getLastName());
-        System.out.println("Date of Birth:" + profile.getDateOfBirth());
-        System.out.println("Username: " + profile.getUsername());
-        System.out.println("Password: " + profile.getPassword());
-        System.out.println("Email: " + profile.getEmail());
+        model.addAttribute("fullname", attr);
+        return "/profile-registered";
+    }
 
-        return "redirect:/";
+    @PostMapping
+    public String handleCreateProfileForm(@ModelAttribute("newProfile") CreateProfile profile, RedirectAttributes attrs)
+    {
+//        System.out.println("First name: " + profile.getFirstName());
+//        System.out.println("Last name: " + profile.getLastName());
+//        System.out.println("Date of Birth:" + profile.getDateOfBirth());
+//        System.out.println("Username: " + profile.getUsername());
+//        System.out.println("Password: " + profile.getPassword());
+//        System.out.println("Email: " + profile.getEmail());
+        attrs.addFlashAttribute("name", profile.getFirstName() + " " + profile.getLastName());
+        return "redirect:/createprofile/welcome";
     }
 }
