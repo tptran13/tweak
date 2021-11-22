0c161bf64ac90e0491871ecbb38ebb633c19ae1a
Tho Tran <tptran13@neiu.edu>
2021-11-21 18:43:41 -0600

    Update view posts

--- src/main/java/edu/neiu/tweak/controller/CreateHackPostController.java
+++ src/main/java/edu/neiu/tweak/controller/CreateHackPostController.java
@@ -2,13 +2,11 @@ package edu.neiu.tweak.controller;
 
 import edu.neiu.tweak.data.HackPostRepository;
 import edu.neiu.tweak.model.CreateHackPost;
-import edu.neiu.tweak.model.CreateProfile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 
@@ -36,7 +34,7 @@ public class CreateHackPostController
     {
         CreateHackPost post = this.postRepo.findById(id).get();
         model.addAttribute("mypost", post);
-        return "view-mypost";
+        return "view-editpost";
     }
 
     @GetMapping("/deletepost/{id}")
@@ -63,7 +61,7 @@ public class CreateHackPostController
     {
         if(error.hasErrors())
         {
-            return "view-mypost";
+            return "view-editpost";
         }
 
         CreateHackPost originalPost = this.postRepo.findById(id).get();
