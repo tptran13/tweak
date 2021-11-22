0e70c8de487a15d4b55b5102ebf6ced24fb6b728
Tho Tran <tptran13@neiu.edu>
2021-11-21 21:27:37 -0600

    added security config and user account controller

---
+++ src/main/java/edu/neiu/tweak/controller/UserAccountController.java
@@ -0,0 +1,16 @@
+package edu.neiu.tweak.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/useraccount")
+public class UserAccountController
+{
+    @GetMapping
+    public String showUserAccount()
+    {
+        return "view-useraccount";
+    }
+}
