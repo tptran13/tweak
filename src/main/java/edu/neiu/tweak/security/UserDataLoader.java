4d95b5bccd68257de9dcb4e2a26a8ed616333e64
Tho Tran <tptran13@neiu.edu>
2021-11-28 23:57:09 -0600

    Created UserDataLoader
    Registration form was created prior to hw10.

--- src/main/java/edu/neiu/tweak/security/UserDataLoader.java
+++ src/main/java/edu/neiu/tweak/security/UserDataLoader.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-
 import java.util.Set;
 
 @Component
